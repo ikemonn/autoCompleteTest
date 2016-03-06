706418226879b4c963b8841804ee572edc92fa6f
ikemoto <ikemoto.daiki@gmail.com>
2016-03-07 07:14:05 +0900

    option追加

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -7,9 +7,9 @@ var atwhoOptions = {
   data: testData
 }
 
-jQuery =>
-    console.log("ロード終了");
-    // $('textarea').atwho(atwhoOptions);
+// 補完内容のリフレッシュ
+function refreshContents() {
+    console.log("変更します");
     chrome.storage.sync.get(null, function(items) {
         var allKeys = Object.keys(items);
         console.log(items);
@@ -23,59 +23,21 @@ jQuery =>
         console.log(testData);
         $('textarea').atwho(atwhoOptions);
     });
+}
+
+jQuery =>
+    console.log("ロード終了");
+    refreshContents();
 
 
 
-    console.log("ロード終了２");
+// 補完項目が変更されたら内容をリフレッシュ
+chrome.runtime.onMessage.addListener(
+  function(request, sender, sendResponse) {
+    console.log("変更を通知した");
+    if (request.greeting == "hello")
+      testData = [];
+      $("textarea").unbind( "atwho", "**" );
+      refreshContents();
 
-
-  // callbacks:
-  //   matcher: (flag, subtext) ->
-  //     regexp = new XRegExp("(\\s+|^)" + flag + "([\\p{L}_-]+)$", "gi")
-  //     match = regexp.exec(subtext)
-  //     return null  unless match and match.length >= 2
-  //     match[2]
-  //
-  //   remote_filter: (query, callback) ->
-  //     return unless query || query.length < 4
-  //     kind = query[0].toLowerCase()
-  //     query = query.slice(1)
-  //     if kind is "k"
-  //       if query
-  //         $.getJSON root_url + "/api/phrases.json",
-  //           text: query
-  //         ,(data) ->
-  //           phrases = $.map data, (phrase) ->
-  //             {
-  //               'id': phrase.id
-  //               'text': phrase.text
-  //               'icon': chrome.extension.getURL('../images/leaf.png')
-  //             }
-  //           callback phrases
-  //     else if kind is "t"
-  //       if query
-  //         $.getJSON root_url + "/api/phrases.json",
-  //           tag: query
-  //         ,(data) ->
-  //           phrases = $.map data, (phrase) ->
-  //             {
-  //               'id': phrase.id
-  //               'text': phrase.text
-  //               'icon': chrome.extension.getURL('../images/tag.png')
-  //             }
-  //           callback phrases
-
-  // var completeData = [
-  //     {name: "デプロイ1", content: "deploy"},
-  //     {name: "ssh1", content: "sshContent"},
-  //     {name: "tunnel1", content: "tunnelContent"},
-  //     {name: "デプロイ2", content: "deploy"},
-  //     {name: "ssh2", content: "sshContent"},
-  //     {name: "tunnel2", content: "tunnelContent"},
-  //     {name: "デプロイ3", content: "deploy"},
-  //     {name: "ssh3", content: "sshContent"},
-  //     {name: "tunnel3", content: "tunnelContent"},
-  //     {name: "デプロイ4", content: "deploy"},
-  //     {name: "ssh4", content: "sshContent"},
-  //     {name: "tunnel4", content: "tunnelContent"}
-  // ];
+});
--- app/scripts/popup.js
+++ app/scripts/popup.js
@@ -8,22 +8,68 @@ var dataOptions = {
 };
 
 
-// Tableに表示
-chrome.storage.sync.get(null, function(items) {
-    var allKeys = Object.keys(items);
-    console.log("popup_items: ", items);
-    console.log("popup_allKeys: ",allKeys);
-    _.map(allKeys, function(key){
-        $('.completeDataTable tbody').append("<tr><td>" + key + "</td><td>" + items[key] + "</td><td><input type='button' class='deleteBtn' value='削除'></input></td></tr>");
+createTable();
+saveContent();
+
+
+
+
+
+// 補完内容の変更通知
+chrome.storage.onChanged.addListener(function(changes, namespace) {
+    console.log("変更があったよ");
+    chrome.tabs.query({active: true, currentWindow: true}, function(tabs) {
+        chrome.tabs.sendMessage(tabs[0].id, {greeting: "hello"}, function(response) {
+            console.log(response.farewell);
+        });
     });
-    $('.deleteBtn').click(function(){
+});
+
+function createTable() {
+    console.log("createTable");
+    chrome.storage.sync.get(null, function(items) {
+        var allKeys = Object.keys(items);
+        console.log("popup_items: ", items);
+        console.log("popup_allKeys: ",allKeys);
+        _.map(allKeys, function(key){
+            $('.completeDataTable tbody').append("<tr><td>" + key + "</td><td>" + items[key] + "</td><td><input type='button' class='deleteBtn' value='削除'></input></td></tr>");
+        });
+        deleteContent();
+    });
+}
+
+function refreshTable() {
+    $(".completeDataTable tbody tr").remove();
+    // Tableに表示
+    createTable();
+
+}
+
+function saveContent() {
+    $('#saveBtn').click(function() {
+        var saveData = {};
+        saveData[$("#nameBox").val()] = $("#commandBox").val();
+
+        chrome.storage.sync.set(saveData, function(){
+            console.log("save finish");
+            $("#nameBox").val('');
+            $("#commandBox").val('');
+            refreshTable();
+        });
+    });
+}
+
+
+function deleteContent() {
+    $('.deleteBtn').click(function() {
         console.log('deleteBtn clicked');
         //クリックで親要素ごと削除する
         console.log($(this).parent().parent().children().eq(0).text());
         var deleteKey = $(this).parent().parent().children().eq(0).text();
         chrome.storage.sync.remove(deleteKey, function(){
             console.log("delete finish");
+            refreshTable();
         });
         //$(this).parent().parent().remove();
     });
-});
+}
